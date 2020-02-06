09ae8f5f7079f7e7fa0aaf9ad4d8eb15ca671f5a
Mihail Chugunov <mihail.chugunov@simbirsoft.com>
2020-02-06 11:27:27 +0400

    Promise resolve and reject for actionViewIntent

--- android/src/main/java/com/RNFetchBlob/RNFetchBlob.java
+++ android/src/main/java/com/RNFetchBlob/RNFetchBlob.java
@@ -111,10 +111,10 @@ public class RNFetchBlob extends ReactContextBaseJavaModule {
             Uri uriForFile = FileProvider.getUriForFile(getCurrentActivity(),
                     this.getReactApplicationContext().getPackageName() + ".provider", new File(path));
 
+            Intent intent = new Intent(Intent.ACTION_VIEW);
             if (Build.VERSION.SDK_INT >= 24) {
                 // Create the intent with data and type
-                Intent intent = new Intent(Intent.ACTION_VIEW)
-                        .setDataAndType(uriForFile, mime);
+                intent.setDataAndType(uriForFile, mime);
 
                 // Set flag to give temporary permission to external app to use FileProvider
                 intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
@@ -123,15 +123,16 @@ public class RNFetchBlob extends ReactContextBaseJavaModule {
 
                 // Validate that the device can open the file
                 PackageManager pm = getCurrentActivity().getPackageManager();
-                if (intent.resolveActivity(pm) != null) {
-                    this.getReactApplicationContext().startActivity(intent);
-                }
-
             } else {
-                Intent intent = new Intent(Intent.ACTION_VIEW)
-                        .setDataAndType(Uri.parse("file://" + path), mime).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.setDataAndType(Uri.parse("file://" + path), mime).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            }
 
+            PackageManager pm = getCurrentActivity().getPackageManager();
+            if (intent.resolveActivity(pm) != null) {
                 this.getReactApplicationContext().startActivity(intent);
+                promise.resolve(true);
+            } else {
+                promise.reject("ENOAPP", "No app installed for " + mime);
             }
             ActionViewVisible = true;
 
