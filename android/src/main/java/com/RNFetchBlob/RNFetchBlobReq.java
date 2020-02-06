9a168bf9960bee402668deefff2629a66d8c02bc
Mihail Chugunov <mihail.chugunov@simbirsoft.com>
2020-02-06 13:36:29 +0400

    Add try catch

--- android/src/main/java/com/RNFetchBlob/RNFetchBlobReq.java
+++ android/src/main/java/com/RNFetchBlob/RNFetchBlobReq.java
@@ -598,7 +598,11 @@ public class RNFetchBlobReq extends BroadcastReceiver implements Runnable {
                 } catch (ClassCastException ex) {
                     // unexpected response type
                     if (responseBody != null) {
-                        callback.invoke("Unexpected FileStorage response file: " + responseBody.string(), null);
+                        try {
+                            callback.invoke("Unexpected FileStorage response file: " + responseBody.string(), null);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
                     } else {
                         callback.invoke("Unexpected FileStorage response with no file.", null);
                     }
