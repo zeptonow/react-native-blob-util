7a40700767f24ee2d9dfd61d9518f4a2626a497e
Ron Radtke <ron.radtke@hawk-intech.com>
2021-08-04 09:06:51 +0200

    Merge remote-tracking branch 'origin/master' into develop

--- polyfill/Event.js
+++ polyfill/Event.js
@@ -4,8 +4,8 @@
 
 export default class Event {
 
-  constructor() {
+    constructor() {
 
-  }
+    }
 
 }
--- polyfill/Fetch.js
+++ polyfill/Fetch.js
@@ -1,10 +1,8 @@
-import {NativeModules} from 'react-native';
 import Log from '../utils/log.js'
-import fs from '../fs'
-import unicode from '../utils/unicode'
 import Blob from './Blob'
 
-const ReactNativeBlobUtil = NativeModules.ReactNativeBlobUtil
+import ReactNativeBlobUtil from "../index";
+
 const log = new Log('FetchPolyfill')
 
 log.disable()
@@ -12,133 +10,133 @@ log.disable()
 
 export default class Fetch {
 
-  constructor(config:ReactNativeBlobUtilConfig) {
-    Object.assign(this, new ReactNativeBlobUtilFetchPolyfill(config))
-  }
+    constructor(config: ReactNativeBlobUtilConfig) {
+        Object.assign(this, new ReactNativeBlobUtilFetchPolyfill(config))
+    }
 
 }
 
 class ReactNativeBlobUtilFetchPolyfill {
 
-  constructor(config:ReactNativeBlobUtilConfig) {
-    this.build = () => (url, options = {}) => {
+    constructor(config: ReactNativeBlobUtilConfig) {
+        this.build = () => (url, options = {}) => {
 
-      let body = options.body
-      let promise = Promise.resolve()
-      let blobCache = null
+            let body = options.body
+            let promise = Promise.resolve()
+            let blobCache = null
 
-      options.headers = options.headers || {}
-      let ctype = options['Content-Type'] || options['content-type']
-      let ctypeH = options.headers['Content-Type'] || options.headers['content-type']
-      options.headers['Content-Type'] = ctype || ctypeH
-      options.headers['content-type'] = ctype || ctypeH
-      options.method = options.method || 'GET'
-      if(body) {
-        // When the request body is an instance of FormData, create a Blob cache
-        // to upload the body.
-        if(body instanceof FormData) {
-          log.verbose('convert FormData to blob body')
-          promise = Blob.build(body).then((b) => {
-            blobCache = b
-            options.headers['Content-Type'] = 'multipart/form-data;boundary=' + b.multipartBoundary
-            return Promise.resolve(ReactNativeBlobUtil.wrap(b._ref))
-          })
+            options.headers = options.headers || {}
+            let ctype = options['Content-Type'] || options['content-type']
+            let ctypeH = options.headers['Content-Type'] || options.headers['content-type']
+            options.headers['Content-Type'] = ctype || ctypeH
+            options.headers['content-type'] = ctype || ctypeH
+            options.method = options.method || 'GET'
+            if (body) {
+                // When the request body is an instance of FormData, create a Blob cache
+                // to upload the body.
+                if (body instanceof FormData) {
+                    log.verbose('convert FormData to blob body')
+                    promise = Blob.build(body).then((b) => {
+                        blobCache = b
+                        options.headers['Content-Type'] = 'multipart/form-data;boundary=' + b.multipartBoundary
+                        return Promise.resolve(ReactNativeBlobUtil.wrap(b._ref))
+                    })
+                }
+                // When request body is a Blob, use file URI of the Blob as request body.
+                else if (body.isReactNativeBlobUtilPolyfill)
+                    promise = Promise.resolve(ReactNativeBlobUtil.wrap(body.blobPath))
+                else if (typeof body !== 'object' && options.headers['Content-Type'] !== 'application/json')
+                    promise = Promise.resolve(JSON.stringify(body))
+                else if (typeof body !== 'string')
+                    promise = Promise.resolve(body.toString())
+                // send it as-is, leave the native module decide how to send the body.
+                else
+                    promise = Promise.resolve(body)
+            }
+            // task is a progress reportable and cancellable Promise, however,
+            // task.then is not, so we have to extend task.then with progress and
+            // cancel function
+            let progressHandler, uploadHandler, cancelHandler
+            let statefulPromise = promise
+                .then((body) => {
+                    let task = ReactNativeBlobUtil.config(config)
+                        .fetch(options.method, url, options.headers, body)
+                    if (progressHandler)
+                        task.progress(progressHandler)
+                    if (uploadHandler)
+                        task.uploadProgress(uploadHandler)
+                    if (cancelHandler)
+                        task.cancel()
+                    return task.then((resp) => {
+                        log.verbose('response', resp)
+                        // release blob cache created when sending request
+                        if (blobCache !== null && blobCache instanceof Blob)
+                            blobCache.close()
+                        return Promise.resolve(new ReactNativeBlobUtilFetchResponse(resp))
+                    })
+                })
+
+            // extend task.then progress with report and cancelling functions
+            statefulPromise.progress = (fn) => {
+                progressHandler = fn
+            }
+            statefulPromise.uploadProgress = (fn) => {
+                uploadHandler = fn
+            }
+            statefulPromise.cancel = () => {
+                cancelHandler = true
+                if (task.cancel)
+                    task.cancel()
+            }
+
+            return statefulPromise
+
         }
-        // When request body is a Blob, use file URI of the Blob as request body.
-        else if (body.isReactNativeBlobUtilPolyfill)
-          promise = Promise.resolve(ReactNativeBlobUtil.wrap(body.blobPath))
-        else if (typeof body !== 'object' && options.headers['Content-Type'] !== 'application/json')
-          promise = Promise.resolve(JSON.stringify(body))
-        else if (typeof body !== 'string')
-          promise = Promise.resolve(body.toString())
-        // send it as-is, leave the native module decide how to send the body.
-        else
-          promise = Promise.resolve(body)
-      }
-      // task is a progress reportable and cancellable Promise, however,
-      // task.then is not, so we have to extend task.then with progress and
-      // cancel function
-      let progressHandler, uploadHandler, cancelHandler
-      let statefulPromise = promise
-          .then((body) => {
-            let task = ReactNativeBlobUtil.config(config)
-              .fetch(options.method, url, options.headers, body)
-            if(progressHandler)
-              task.progress(progressHandler)
-            if(uploadHandler)
-              task.uploadProgress(uploadHandler)
-            if(cancelHandler)
-              task.cancel()
-            return task.then((resp) => {
-              log.verbose('response', resp)
-              // release blob cache created when sending request
-              if(blobCache !== null && blobCache instanceof Blob)
-                blobCache.close()
-              return Promise.resolve(new ReactNativeBlobUtilFetchResponse(resp))
-            })
-          })
-
-      // extend task.then progress with report and cancelling functions
-      statefulPromise.progress = (fn) => {
-        progressHandler = fn
-      }
-      statefulPromise.uploadProgress = (fn) => {
-        uploadHandler = fn
-      }
-      statefulPromise.cancel = () => {
-        cancelHandler = true
-        if(task.cancel)
-          task.cancel()
-      }
-
-      return statefulPromise
-
     }
-  }
 
 }
 
 class ReactNativeBlobUtilFetchResponse {
 
-  constructor(resp:FetchBlobResponse) {
-    let info = resp.info()
-    this.headers = info.headers
-    this.ok = info.status >= 200 && info.status <= 299,
-    this.status = info.status
-    this.type = 'basic'
-    this.bodyUsed = false
-    this.resp = resp
-    this.rnfbRespInfo = info
-    this.rnfbResp = resp
-  }
+    constructor(resp: FetchBlobResponse) {
+        let info = resp.info()
+        this.headers = info.headers
+        this.ok = info.status >= 200 && info.status <= 299,
+            this.status = info.status
+        this.type = 'basic'
+        this.bodyUsed = false
+        this.resp = resp
+        this.rnfbRespInfo = info
+        this.rnfbResp = resp
+    }
 
-  rawResp() {
-    return Promise.resolve(this.rnfbResp)
-  }
+    rawResp() {
+        return Promise.resolve(this.rnfbResp)
+    }
 
-  arrayBuffer(){
-    log.verbose('to arrayBuffer', this.rnfbRespInfo)
-    this.bodyUsed = true
-    return readArrayBuffer(this.rnfbResp, this.rnfbRespInfo)
-  }
+    arrayBuffer() {
+        log.verbose('to arrayBuffer', this.rnfbRespInfo)
+        this.bodyUsed = true
+        return readArrayBuffer(this.rnfbResp, this.rnfbRespInfo)
+    }
 
-  text() {
-    log.verbose('to text', this.rnfbResp, this.rnfbRespInfo)
-    this.bodyUsed = true
-    return readText(this.rnfbResp, this.rnfbRespInfo)
-  }
+    text() {
+        log.verbose('to text', this.rnfbResp, this.rnfbRespInfo)
+        this.bodyUsed = true
+        return readText(this.rnfbResp, this.rnfbRespInfo)
+    }
 
-  json() {
-    log.verbose('to json', this.rnfbResp, this.rnfbRespInfo)
-    this.bodyUsed = true
-    return readJSON(this.rnfbResp, this.rnfbRespInfo)
-  }
+    json() {
+        log.verbose('to json', this.rnfbResp, this.rnfbRespInfo)
+        this.bodyUsed = true
+        return readJSON(this.rnfbResp, this.rnfbRespInfo)
+    }
 
-  blob() {
-    log.verbose('to blob', this.rnfbResp, this.rnfbRespInfo)
-    this.bodyUsed = true
-    return readBlob(this.rnfbResp, this.rnfbRespInfo)
-  }
+    blob() {
+        log.verbose('to blob', this.rnfbResp, this.rnfbRespInfo)
+        this.bodyUsed = true
+        return readBlob(this.rnfbResp, this.rnfbRespInfo)
+    }
 }
 
 /**
@@ -147,20 +145,20 @@ class ReactNativeBlobUtilFetchResponse {
  * @param  {ReactNativeBlobUtilResponseInfo} info Response informations.
  * @return {Promise<Array>}
  */
-function readArrayBuffer(resp, info):Promise<Array> {
-  switch (info.rnfbEncode) {
-    case 'path':
-      return resp.readFile('ascii')
-      break
-    default:
-      let buffer = []
-      let str = resp.text()
-      for (let i in str) {
-        buffer[i] = str.charCodeAt(i);
-      }
-      return Promise.resolve(buffer)
-      break
-  }
+function readArrayBuffer(resp, info): Promise<Array> {
+    switch (info.rnfbEncode) {
+        case 'path':
+            return resp.readFile('ascii')
+            break
+        default:
+            let buffer = []
+            let str = resp.text()
+            for (let i in str) {
+                buffer[i] = str.charCodeAt(i);
+            }
+            return Promise.resolve(buffer)
+            break
+    }
 }
 
 /**
@@ -169,18 +167,18 @@ function readArrayBuffer(resp, info):Promise<Array> {
  * @param  {ReactNativeBlobUtilResponseInfo} info Response informations.
  * @return {Promise<string>}
  */
-function readText(resp, info):Promise<string> {
-  switch (info.rnfbEncode) {
-    case 'base64':
-      return Promise.resolve(resp.text())
-      break
-    case 'path':
-      return resp.text()
-      break
-    default:
-      return Promise.resolve(resp.text())
-      break
-  }
+function readText(resp, info): Promise<string> {
+    switch (info.rnfbEncode) {
+        case 'base64':
+            return Promise.resolve(resp.text())
+            break
+        case 'path':
+            return resp.text()
+            break
+        default:
+            return Promise.resolve(resp.text())
+            break
+    }
 }
 
 
@@ -190,9 +188,9 @@ function readText(resp, info):Promise<string> {
  * @param  {ReactNativeBlobUtilResponseInfo} info Response informations.
  * @return {Promise<Blob>}
  */
-function readBlob(resp, info):Promise<Blob> {
-  log.verbose('readBlob', resp, info)
-  return resp.blob()
+function readBlob(resp, info): Promise<Blob> {
+    log.verbose('readBlob', resp, info)
+    return resp.blob()
 }
 
 /**
@@ -201,14 +199,14 @@ function readBlob(resp, info):Promise<Blob> {
  * @param  {ReactNativeBlobUtilResponseInfo} info Response informations.
  * @return {Promise<object>}
  */
-function readJSON(resp, info):Promise<object> {
-  log.verbose('readJSON', resp, info)
-  switch (info.rnfbEncode) {
-    case 'base64':
-      return Promise.resolve(resp.json())
-    case 'path':
-      return resp.json()
-    default:
-      return Promise.resolve(resp.json())
-  }
+function readJSON(resp, info): Promise<object> {
+    log.verbose('readJSON', resp, info)
+    switch (info.rnfbEncode) {
+        case 'base64':
+            return Promise.resolve(resp.json())
+        case 'path':
+            return resp.json()
+        default:
+            return Promise.resolve(resp.json())
+    }
 }
